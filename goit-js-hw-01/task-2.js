78f652b081fbe169158640b52e3a32b067cc8ae6
dima-kulikov <53553661+dima-kulikov@users.noreply.github.com>
2020-01-22 23:11:38 +0200

    saa
    sacas

--- goit-js-hw-01/task-2.js
+++ goit-js-hw-01/task-2.js
@@ -9,3 +9,15 @@ if(isInRange){
 if(!isInRange){
     console.log(`Заказ оформлен, с вами свяжется менеджер`);   
 }
+
+// ВАРИАНТ ------- 2----
+
+const total = 100;
+let ordered = 30
+
+if(total < ordered){
+    console.log(`На складе недостаточно твоаров!`);   
+}
+else{
+    console.log(`Заказ оформлен, с вами свяжется менеджер`);   
+}

d8334ce27f22866742b731b44e5bee667329d6f4
dima-kulikov <skaizy44@gmail.com>
2020-02-06 18:52:39 +0200

    11
    11

---
+++ goit-js-hw-02/task-1.js
@@ -0,0 +1,18 @@
+
+const logItems = function(array) {
+    const nomera = array.length;
+    
+    for (let i = 0; i < nomera; i++) { 
+        console.log(`номер: ${i+1}, значение ${array[i]}`)
+      }
+          return logItems
+  };
+ 
+    logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+    logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+    
+
+
+
+
+

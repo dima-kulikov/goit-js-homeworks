e0f3a9ba3b86d3ff367a99ff0d917cb32e831f52
dima-kulikov <skaizy44@gmail.com>
2020-10-28 21:55:06 +0200

    1
    1

---
+++ goit-js-hw-03/practic-1.js
@@ -0,0 +1,31 @@
+// const product = {
+//     name: "Dima",
+//     description:"учеба js курсы онлайн.",
+//     price:30,
+// };
+// const key = 'price';
+// console.log(product.price)
+// console.log(product)
+// delete product.name;
+// console.log(product)
+
+const makeproduct = (name, prise, desc) =>{
+    // return{
+    //     name:name,
+    //     price:prise,
+    //     desc:desc,
+    // }
+    return{
+        name,
+        prise,
+        desc,
+        color: 'red',
+    }
+}
+
+
+console.log("dima", 50, "goit");
+console.log(makeproduct("dima", 50, "goit"))
+
+// 20 минута   https://www.youtube.com/watch?time_continue=1206&v=RU6l75P042A&feature=emb_logo
+

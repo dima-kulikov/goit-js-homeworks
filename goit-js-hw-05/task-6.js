d8e480266170b5a7f7454e917544a047520338c5
dima-kulikov <skaizy44@gmail.com>
2021-03-30 23:15:24 +0300

    Update task-6.js
    s

--- goit-js-hw-05/task-6.js
+++ goit-js-hw-05/task-6.js
@@ -35,18 +35,33 @@
 //   console.log(mas)
 // --------------------------------------------
 
-const mas = [1,2,3,4,5];
+// const mas = [1,2,3,4,5];
 
-const number = (array,value)=>{
-    const masNew = [];
-    for(let i=0; i<array.length; i++){
-        masNew.push(array[i] * value);
-    }
-    return masNew
-  };
-const masNew2 = number(mas, 2);
+// const number = (array,value)=>{
+//     const masNew = [];
+//     for(let i=0; i<array.length; i++){
+//         masNew.push(array[i] * value);
+//     }
+//     return masNew
+//   };
+// const masNew2 = number(mas, 2);
 
 
-  number(mas, 2);
-  console.log(mas)
-  console.log(masNew2)
+//   number(mas, 2);
+//   console.log(mas)
+//   console.log(masNew2)
+// ---------------------------------------------------
+
+// 2. Array.prototype.forEach()
+
+const numbers = [1, 2, 3];
+
+// Классический for
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(numbers[i]);
+// }
+
+// Функциональный forEach
+numbers.forEach(num => console.log(num));
+// Указываем параметр idx если нужен доступ к счетчику
+numbers.forEach((num, idx) => console.log(`index ${idx}, value ${num}`));

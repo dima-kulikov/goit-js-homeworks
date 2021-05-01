6a5995e3898e667c34b10a45af6e9b4ddf7110ec
dima-kulikov <skaizy44@gmail.com>
2021-05-01 22:45:48 +0300

    23
    ds

---
+++ goit-js-hw-07/js/practic.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.button')
+
+button.textContent ='Heloo!!!!'
+
+
+const mi = document.querySelectorAll('.menu-item');
+console.log(mi)
+mi.textContent ='123456!!!!'

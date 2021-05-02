afb466a96598721f8494f5db7f537460684ef018
dima-kulikov <skaizy44@gmail.com>
2021-05-02 23:03:29 +0300

    s
    s

--- goit-js-hw-07/js/practic.js
+++ goit-js-hw-07/js/practic.js
@@ -1,10 +1,22 @@
 const button = document.querySelector('.button')
+const buttonjs = document.querySelector('.js-btn');
+
+
 
 button.textContent ='Heloo!!!!'
-
 
 const mi = document.querySelectorAll('.menu-item');
 console.log(mi)
 
 const image = document.querySelector('.avatar');
-console.log(image)
+
+
+buttonjs.addEventListener('click', nextimg);
+
+image.alt = 'Магазин Днипро-М';
+image.title = 'Магазин Днипро-М';
+
+
+function nextimg(){
+    image.src ='https://klike.net/uploads/posts/2018-11/1543310656_14.jpg';
+}

bb10fc03518025cccf9ed60cfa329add6278ce57
dima-kulikov <skaizy44@gmail.com>
2021-05-06 22:18:11 +0300

    111
    2121

--- goit-js-hw-07/js/practic.js
+++ goit-js-hw-07/js/practic.js
@@ -1,23 +1,25 @@
 const button = document.querySelector('.button')
-const buttonjs = document.querySelector('.js-btn');
+const buttonjs = document.querySelector('button[data-action="magic"]');
 const homeMenuActiv = document.querySelector('.menu-item');
-
+const image = document.querySelector('.logo');
 
 button.textContent ='Heloo!!!!'
 
 const mi = document.querySelectorAll('.menu-item');
 console.log(mi)
 
-const image = document.querySelector('.avatar');
 
 
-buttonjs.addEventListener('click', className);
 
-image.alt = 'Магазин Днипро-М';
-image.title = 'Магазин Днипро-М';
+buttonjs.addEventListener('click',()=>{
+     className();
+     image.setAttribute('src', 'https://3dnews.ru/assets/external/illustrations/2019/12/04/999060/dims%20(3).jpg');
+     image.setAttribute('title', 'HELLO!')
+});
+
 
 
 function className(){
-    homeMenuActiv.classList.add('active')
+    homeMenuActiv.classList.toggle('active')
 };
 

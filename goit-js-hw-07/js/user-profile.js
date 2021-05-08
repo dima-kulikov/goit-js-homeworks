1f8211d8e9bc90340efd9dacaef39092344acbd3
dima-kulikov <skaizy44@gmail.com>
2021-05-08 15:13:59 +0300

    Update user-profile.js
    вывы

---
+++ goit-js-hw-07/js/user-profile.js
@@ -0,0 +1,12 @@
+// createUserProfile(user);
+
+
+// function createUserProfile(){
+//     const container = document.createElement('div');
+
+//     console.log(container)
+// }
+
+const a = 10;
+
+console.log(a)

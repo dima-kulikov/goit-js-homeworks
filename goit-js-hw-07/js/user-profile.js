54944b9178bc57c155436bcc0cc8af0027c8daa5
dima-kulikov <skaizy44@gmail.com>
2021-05-09 23:30:35 +0300

    grg
    greg

--- goit-js-hw-07/js/user-profile.js
+++ goit-js-hw-07/js/user-profile.js
@@ -1,12 +1,47 @@
-// createUserProfile(user);
+const user = {
+    name:'Dima kl',
+    phone:'121-245-45-54',
+    email:'dima@mail.com',
+    avatar:'https://www.meme-arsenal.com/memes/cbfd4797382778baf1f41b8439399262.jpg',
+    address:{
+        country:'USA',
+        city:'NY'
+    }
+};
 
 
-// function createUserProfile(){
-//     const container = document.createElement('div');
+const profileContainer = document.querySelector('.profile-container');
 
-//     console.log(container)
-// }
+const profile = createUserProfile(user);
 
-const a = 10;
+profileContainer.appendChild(profile);
 
-console.log(a)
+
+function createUserProfile({name, phone, email, avatar, address}){
+    const container = document.createElement('div');
+    container.classList.add('user-profile');
+
+    const image = document.createElement('img');
+    image.src = avatar;
+    image.alt = 'аватарка';
+    image.title ='Фото карточки';
+    image.classList.add('avatars');
+
+    const infoList = document.createElement("ul");
+    infoList.classList.add('info');
+
+    const nameField = document.createElement('li');
+    const label = document.createElement('b');
+    
+
+
+    // container.appendChild(image);
+    // container.appendChild(infoList);
+    container.append(image, infoList);
+
+    console.log(container);
+    return container;
+
+    
+}
+

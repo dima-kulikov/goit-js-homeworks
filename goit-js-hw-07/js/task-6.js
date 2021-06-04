2cd2edefe28195c104bf34caaf2c35d98aba37da
dima-kulikov <53553661+dima-kulikov@users.noreply.github.com>
2021-06-04 16:42:49 +0300

    Update task-6.js
    gjj

--- goit-js-hw-07/js/task-6.js
+++ goit-js-hw-07/js/task-6.js
@@ -1,15 +1,29 @@
 let inputText = document.querySelector('#validation-input');
 const dataLength = document.querySelector("[data-length]");
+// inputText.oninput = function() {
+//     if(inputText.value.length == inputText.dataset.length){
+//         console.log('длина 6')
+//     }
+
+//     else{
+//         console.log('cccдлина не 6')
+
+//     }
+// }
+// console.log(inputText.dataset.length)
 inputText.oninput = function() {
-    if(inputText.value.length == 0){
-        console.log('зззззззззз')
+inputText.addEventListener('focus', ()=>{
+    if(inputText.value.length == inputText.dataset.length){
+        console.log('длина 6')
+        inputText.classList.add('valid');
+        inputText.classList.remove('invalid');
     }
-    else if(inputText.value.length == inputText.dataset.length){
-        console.log('work')
-    }
+
     else{
-        console.log('ccccc')
-        console.log(inputText.value.length)
+        console.log('cccдлина не 6')
+        inputText.classList.add('invalid');
+        inputText.classList.remove('valid');
     }
-}
-console.log(inputText.dataset.length)
+});
+};
+// .classList.add('focus');

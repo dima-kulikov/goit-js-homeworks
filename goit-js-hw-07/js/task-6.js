dc6bad16c4e36388bedbc85e792177be34960c88
dima-kulikov <skaizy44@gmail.com>
2021-06-07 16:39:02 +0300

    вы
    ыв

--- goit-js-hw-07/js/task-6.js
+++ goit-js-hw-07/js/task-6.js
@@ -10,20 +10,46 @@ const dataLength = document.querySelector("[data-length]");
 
 //     }
 // }
-// console.log(inputText.dataset.length)
-inputText.oninput = function() {
-inputText.addEventListener('focus', ()=>{
+// console.log(inputText.dataset.length)0
+
+const validate = () => {
     if(inputText.value.length == inputText.dataset.length){
         console.log('длина 6')
         inputText.classList.add('valid');
         inputText.classList.remove('invalid');
     }
+    // else if(inputText.value.length == ''){
+    //     inputText.classList.remove('invalid');
+    //     inputText.classList.remove('valid');
+    // }
 
     else{
         console.log('cccдлина не 6')
         inputText.classList.add('invalid');
         inputText.classList.remove('valid');
     }
-});
-};
+}
+
+inputText.addEventListener('input', validate);
+inputText.addEventListener('focus', validate);
+inputText.addEventListener('blur', validate);
+
+
+// inputText.oninput = function() {
+//     console.log( inputText.value)
+    
+// inputText.addEventListener('blur', ()=>{
+//     if(inputText.value.length == inputText.dataset.length){
+//         console.log('длина 6')
+//         inputText.classList.add('valid');
+//         inputText.classList.remove('invalid');
+//     }
+
+//     else{
+//         console.log('cccдлина не 6')
+//         inputText.classList.add('invalid');
+//         inputText.classList.remove('valid');
+//     }
+// });
+// };
 // .classList.add('focus');

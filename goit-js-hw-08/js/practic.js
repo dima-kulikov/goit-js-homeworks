3943c28ce646c978ff90b9f2a60369af13565f47
dima-kulikov <53553661+dima-kulikov@users.noreply.github.com>
2021-06-08 23:16:14 +0300

    cdc
    cdcd

---
+++ goit-js-hw-08/js/practic.js
@@ -0,0 +1,18 @@
+const jsNav = document.querySelector('.js-nav');
+
+jsNav.addEventListener('click', chenjColor);
+
+
+function chenjColor(params) {
+    params.preventDefault();
+    console.log(params.target);
+    console.log(params.currentTarget)
+
+const activLink = params.currentTarget.querySelector('.active');
+console.log(activLink);
+if(activLink){
+    activLink.classList.remove('active');
+}
+
+    params.target.classList.add('active');
+}

aff74f9a7f53b15d40ba9420f95c0cb3db9a3e38
dima-kulikov <skaizy44@gmail.com>
2021-06-12 21:00:48 +0300

    j
    jk

--- goit-js-hw-08/js/practic.js
+++ goit-js-hw-08/js/practic.js
@@ -6,16 +6,37 @@ jsNav.addEventListener('click', chenjColor);
 function chenjColor(params) {
     params.preventDefault();
     console.log(params.target);
-    console.log(params.currentTarget)
+    console.log(params.currentTarget);
 
 const activLink = params.currentTarget.querySelector('.active');
 console.log(activLink);
 if(activLink){
     activLink.classList.remove('active');
-}
+};
 
     params.target.classList.add('active');
+};
+
+
+const onEntry = (entries) => {
+    entries.forEach(entry=>{
+        // console.log(entry);
+
+        if(entry.isIntersecting){
+            console.log(entry.target);
+        }
+
+    });
+};
+
+const options ={
+    threshold: 0.6
 }
 
 
-const observer = new ifntersectionObserver(()=>{}, {});
+const observer = new IntersectionObserver(onEntry, options);
+const section = document.querySelectorAll('.section');
+
+section.forEach(section =>{
+    observer.observe(section);
+});
